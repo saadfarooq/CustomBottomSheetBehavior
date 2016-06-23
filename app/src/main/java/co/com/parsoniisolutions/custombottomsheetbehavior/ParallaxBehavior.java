f3742ebe887ceffe930b216273e7a488be1c1950
Saad Farooq <sa@dfarooq.com>
2016-06-23 12:32:48 -0400

    Progress commit

--- app/src/main/java/co/com/parsoniisolutions/custombottomsheetbehavior/ParallaxBehavior.java
+++ app/src/main/java/co/com/parsoniisolutions/custombottomsheetbehavior/ParallaxBehavior.java
@@ -11,6 +11,8 @@ import android.view.View;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParallaxBehavior<V extends View> extends CoordinatorLayout.Behavior<V> {
     @IntDef({STATE_FULL, STATE_COLLAPSED})
@@ -20,13 +22,14 @@ public class ParallaxBehavior<V extends View> extends CoordinatorLayout.Behavior
     private static final int STATE_FULL = 1;
     private static final int STATE_COLLAPSED = 2;
 
+    private final List<StateChangeListener> listeners = new ArrayList<>(2); // init with 2, we're unlikely to have more
+
     @ParallaxBehavior.State
-    private int mState;
+    private int mState = STATE_FULL;
 
     private V mChild;
 
-    public ParallaxBehavior() {
-    }
+    public ParallaxBehavior() {}
 
     public ParallaxBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -41,22 +44,61 @@ public class ParallaxBehavior<V extends View> extends CoordinatorLayout.Behavior
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, V child, View dependency) {
         if (mChild == null) {
-            initValues(child);
+            initValues(child, dependency);
         }
         return false;
     }
 
-    private void initValues(V child) {
+    private void initValues(V child, View dependency) {
         mChild = child;
         mChild.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN && mState == STATE_FULL) {
-
+                    notifyListenersToChangeState(view, STATE_COLLAPSED);
+                    mState = STATE_COLLAPSED;
                 }
                 return false;
             }
         });
+
+        mChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mState == STATE_COLLAPSED) {
+                    notifyListenersToChangeState(v, STATE_FULL);
+                    mState = STATE_FULL;
+                }
+            }
+        });
+
+        if (BehaviorHelper.hasBehavior(dependency, BottomSheetBehaviorGoogleMapsLike.class)) {
+            BottomSheetBehaviorGoogleMapsLike.from(dependency).addBottomSheetCallback(new BottomSheetBehaviorGoogleMapsLike.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View bottomSheet, @BottomSheetBehaviorGoogleMapsLike.State int newState) {
+
+                }
+
+                @Override
+                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                    mChild.setTranslationY(-mChild.getHeight() * slideOffset * 0.6f);
+                }
+            });
+        }
+    }
+
+    public void onStateChangeListener(StateChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    private void notifyListenersToChangeState(View view, @State int targetState) {
+        for (StateChangeListener listener : listeners) {
+            listener.onStateChanged(view, targetState);
+        }
+    }
+
+    public interface StateChangeListener {
+        void onStateChanged(View view, @State int newState);
     }
 
 }
